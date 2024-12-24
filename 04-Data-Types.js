4f9ceabc7edd3a90d8890c7277577ec581e90166
Muhammad Hanzala <91691179+BabriWala@users.noreply.github.com>
2024-12-25 00:24:18 +0600

    object & Arra

---
+++ 04-Data-Types.js
@@ -0,0 +1,86 @@
+// const heroName = "Allu Arjun"; // String
+// console.log(heroName)
+// let heroName;
+/* 
+    JS -> Two Types of Data Type
+    1. Primitive -> Immutable -> Unchangable
+    2. Non Primitive -> Mutable -> Changable
+
+    1. Primitive Data Types -> Seven Types of Data Types. 
+    1.1: String -> "Jamila", 'Jamila', `Jamila`
+    1.2: Number -> 1, 0.5, NAN
+    1.3: Boolean -> true, false
+    1.4: Null -> null
+    1.5: Undefined -> undefined
+
+    // We are not discussing about this two topics -> Advanced Topics
+    1.6: Symbol
+    1.7: BigInt
+
+    2. NonPrimitive Data Types -> Three Types of Data Types. 
+    2.1: Object -> 
+    2.2: Array -> 
+    2.3: Function -> 
+
+*/
+
+// String
+// let name = "John"; // Statement
+// // console.log(name)
+// let greeting = 'Hello!';
+// // console.log(greeting)
+// let message = `${greeting} Welcome dklfdlkf ${name}`;
+// // ${} -> Expression -> Single Value
+// console.log(message)
+
+// Number Data Types
+// let age = 25;
+// // console.log(age)
+// let temprature = 36.6;
+// console.log(temprature)
+// console.log(typeof temprature)
+
+// Data Types Boolean
+// let isEligibleForMarriage = true;
+// console.log(isEligibleForMarriage);
+// console.log(typeof isEligibleForMarriage);
+// let isStudent = false;
+// console.log(isStudent)
+
+// Data Types -> Null
+// let yousufWifesName = null;
+// console.log(yousufWifesName);
+// console.log(typeof yousufWifesName);
+
+
+// Data Types - Undfined 
+// let notAssigned;
+// console.log(notAssigned);
+// console.log(typeof notAssigned);
+
+// Non Primitivve Data Types => Object
+// let mohan = {
+//     // Properties -> Key & Value
+//     fullName: "Mohan Lal",
+//     // Key : Value -> Every Data Types = Properties,
+//     age: 20,
+//     isStudent: true,
+//     isJobHolder: false,
+//     isHeMarried: "Yes",
+// }; 
+
+// console.log(mohan)
+// console.log(mohan.fullName)
+// console.log(mohan.isHeMarried)
+// Dot Notation
+// console.log(mohan["isHeMarried"])
+// Bracket Notation
+
+
+
+// Non Primitivve Data Types => Array
+let friends = ["abdul", "kuddus", "jobbar", false, {fullName: "aleem"} ];
+//              0           1       2           3       4
+// console.log(friends);
+console.log(friends[3])
+
