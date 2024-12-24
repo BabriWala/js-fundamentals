56087e149eddab782d450fd5c10d09382396f7d5
Muhammad Hanzala <91691179+BabriWala@users.noreply.github.com>
2024-12-24 23:19:14 +0600

    Vaiable Let

---
+++ 02-variable-let.js
@@ -0,0 +1,29 @@
+// var husbandOfJamilasName;
+// husbandOfJamilasName = "Yousuf";
+// console.log(husbandOfJamilasName);
+
+// var husbandOfJamilasName = "Yousuf";
+// console.log(husbandOfJamilasName);
+
+// let husbandOfJamilasName;
+// husbandOfJamilasName = "Yousuf";
+// console.log(husbandOfJamilasName);
+
+// let husbandOfJamilasName = "Yousuf";
+// console.log(husbandOfJamilasName);
+
+
+// var husbandOfJamilasName = "Yousuf";
+// console.log(husbandOfJamilasName);
+// var husbandOfJamilasName = "Yousuf Jamila";
+// console.log(husbandOfJamilasName);
+
+// let husbandOfJamilasName = "yousuf";
+// console.log(husbandOfJamilasName);
+// let husbandOfJamilasName = "Yousuf Jamila"
+// console.log(husbandOfJamilasName);
+// husbandOfJamilasName = "Yousuf Jamila"
+// console.log(husbandOfJamilasName);
+
+// Khelar Math -> Khela Dhula
+// node JS Runtime Environment
