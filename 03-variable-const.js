696414de2be5a7b1c395553d7a33f197185f9c7f
Muhammad Hanzala <91691179+BabriWala@users.noreply.github.com>
2024-12-24 23:31:59 +0600

    Const

---
+++ 03-variable-const.js
@@ -0,0 +1,16 @@
+// const wifeOfYousuf = "Jamila Yousuf";
+// console.log(wifeOfYousuf);
+
+const wifeOfYousuf = "Jamila Yousuf";
+// console.log(wifeOfYousuf);
+// wifeOfYousuf = "Mahjabin Yousuf";
+// console.log(wifeOfYousuf);
+
+/* 
+    var -> Initialization, Declare, Reassign, Redeclare 
+    let -> Initialization, Declare, Reassign
+    const -> Initialization 
+
+    Naming Convention -> CamelCase, a-z, A-Z, 0-9, $, _ 
+    Spacing, CaseSensitive -> A - a
+*/
